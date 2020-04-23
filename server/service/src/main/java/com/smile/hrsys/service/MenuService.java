d04f8c119c62b36d096461a507b5908f22a44a65
Tony <chengkaizone@gmail.com>
2020-04-23 22:56:54 +0800

    1. 添加前端代码

--- server/service/src/main/java/com/smile/hrsys/service/MenuService.java
+++ server/service/src/main/java/com/smile/hrsys/service/MenuService.java
@@ -6,6 +6,7 @@ import com.smile.hrsys.model.Menu;
 import com.smile.hrsys.utils.HrUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,6 +29,7 @@ public class MenuService {
         return menuMapper.getAllMenusWithRole();
     }
 
+    @Cacheable(key = "#root.methodName")
     public List<Menu> getAllMenus() {
         return menuMapper.getAllMenus();
     }
--- server/web/src/main/java/com/smile/hrsys/HrsysApplication.java
+++ server/web/src/main/java/com/smile/hrsys/HrsysApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableCaching
@@ -15,6 +16,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class HrsysApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+
+        //$2a$10$ySG2lkvjFHY5O0./CPIE1OI8VJsuKYEzOYzqIa7AJR6sEgSzUFOAm
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String text = encoder.encode("123");
+        System.out.println("text--> " + text);
         SpringApplication.run(HrsysApplication.class, args);
     }
 
